557086742af1917deeda0d47d5ff40560b6e3f7b
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2025-11-07 11:11:26 +0530

    Redis config file update

---
+++ Backend/app/redis_config.py
@@ -0,0 +1,23 @@
+
+import os
+import redis
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", "6379"))
+
+print(f"Using Redis at {redis_host}:{redis_port}")
+
+def get_redis_client() -> redis.Redis:
+    """
+    Get Redis client configured for local Docker Redis
+    """
+    return redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        decode_responses=False
+    )
